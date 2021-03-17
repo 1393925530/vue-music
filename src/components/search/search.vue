36a1cc455a361e25e7fe03f9809e13b19478fa7a
1393925530 <zzc1393925530@gmail.com>
2021-03-18 00:13:05 +0800

    ✨feat(项目): Initial commit

---
+++ src/components/search/search.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    搜索
+  </div>
+</template>
+
+<script>
+export default {
+  props: [],
+
+  components: {},
+
+  data () {
+    return {
+
+    }
+  },
+
+  watch: {
+
+  },
+
+  computed: {
+
+  },
+
+  mounted () {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    歌手
+  </div>
+</template>
+
+<script>
+export default {
+  props: [],
+
+  components: {},
+
+  data () {
+    return {
+
+    }
+  },
+
+  watch: {
+
+  },
+
+  computed: {
+
+  },
+
+  mounted () {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
