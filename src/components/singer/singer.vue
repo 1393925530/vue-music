5cdf5cc5a5db71ba1c7d5397cc078655a2ddb18d
1393925530 <zzc1393925530@gmail.com>
2021-03-18 21:58:57 +0800

    ✨feat(歌手页面开发): 歌手展示/侧边导航/吸顶效果

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="singer" ref="singer"></div>
+  <div class="singer" ref="singer">
+    <list-view :data="singers"></list-view>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
 import {getSingerList} from 'api/singer'
 import {ERR_OK} from 'api/config'
 import Singer from 'common/js/singer'
+import ListView from 'base/listview/listview'
 
 const HOT_NAME = '热门'
 const HOT_SINGER_LEN = 10
@@ -13,7 +16,9 @@ const HOT_SINGER_LEN = 10
 export default {
   props: [],
 
-  components: {},
+  components: {
+    ListView
+  },
 
   data () {
     return {
@@ -41,8 +46,7 @@ export default {
     _getSingerList () {
       getSingerList().then((res) => {
         if (res.code === ERR_OK) {
-          this.singers = res.data.list
-          console.log(this._normalizeSinger(res.data.list))
+          this.singers = this._normalizeSinger(res.data.list)
         }
       })
     },
