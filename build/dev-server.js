9fa38bca5018784b7ac467d6faf3527df8258490
1393925530 <zzc1393925530@gmail.com>
2021-03-25 20:01:37 +0800

    ✨feat(搜索功能):

--- build/dev-server.js
+++ build/dev-server.js
@@ -92,6 +92,22 @@ apiRoutes.get('/toplist', function (req, res) {
   })
 })
 
+apiRoutes.get('/search', function (req, res) {
+  var url = `https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp`
+
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  })
+})
+
 app.use('/api', apiRoutes)
 
 var compiler = webpack(webpackConfig)
--- src/store/actions.js
+++ src/store/actions.js
@@ -24,12 +24,56 @@ export const selectPlay = function ({commit, state}, {list, index}) {
   commit(types.SET_PLAYING_STATE, true)
 }
 
-export const randomPlay  =function ({commit}, {list}) {
+export const randomPlay = function ({commit}, {list}) {
   commit(types.SET_PLAY_MODE, playMode.random)
   commit(types.SET_SEQUENCE_LIST, list)
   let randomList = shuffle(list)
   commit(types.SET_PLAYLIST, randomList)
   commit(types.SET_CURRENT_INDEX, 0)
+  commit(types.SET_FULL_SCREEN, true)
+  commit(types.SET_PLAYING_STATE, true)
+}
+
+export const insertSong = function ({ commit, state }, song) {
+  let playlist = state.playlist.slice()
+  let sequenceList = state.sequenceList.slice()
+  let currentIndex = state.currentIndex
+  // 记录当前歌曲
+  let currentSong = playlist[currentIndex]
+  // 查找当前列表中是否有待插入的歌曲 并返回索引
+  let fpIndex = findIndex(playlist, song)
+  // 插入歌曲 索引加1
+  currentIndex++
+  // 插入这首歌到当前索引位置
+  playlist.splice(currentIndex, 0, song)
+  // 如果已经包含了
+  if (fpIndex > -1) {
+    // 当前插入序号大于列表序号
+    if (currentIndex > fpIndex) {
+      playlist.splice(fpIndex, 1)
+      currentIndex--
+    } else {
+      playlist.splice(fpIndex + 1, 1)
+    }
+  }
+
+  let currentSIndex = findIndex(sequenceList, currentSong) + 1
+
+  let fsIndex = findIndex(sequenceList, song)
+
+  sequenceList.splice(currentSIndex, 0, song)
+
+  if(fsIndex > -1) {
+    if (currentSIndex > fsIndex) {
+      sequenceList.splice(fsIndex, 1)
+    } else {
+      sequenceList.splcie(fsIndex + 1, 1)
+    }
+  }
+
+  commit(types.SET_PLAYLIST, playlist)
+  commit(types.SET_SEQUENCE_LIST, sequenceList)
+  commit(types.SET_CURRENT_INDEX, currentIndex)
   commit(types.SET_FULL_SCREEN, true)
   commit(types.SET_PLAYING_STATE, true)
 }
